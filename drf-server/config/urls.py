fb74316a2244003ecdeb57b39d84ef6b0d65c165
cjy <sapla@naver.com>
2026-04-15 16:18:02 +0900

    fix : 경로수정 및 파일명 수정

--- drf-server/config/urls.py
+++ drf-server/config/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
-from apps.accounts.views import login_view
 
 
 # 작동확인용
 def dashboard(request):
-    return render(request, "dashboard_web.html")
+    return render(request, "dashboard.html")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", login_view),
     path("dashboard/", dashboard),
 ]
