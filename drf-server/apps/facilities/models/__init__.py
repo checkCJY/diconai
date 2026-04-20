77c8d0d84d966f040bd0dc34075511b2ea17cc43
cjy <sapla@naver.com>
2026-04-20 13:26:52 +0900

    refactor : import 경로 수정 v_2( 절대경로로 수정 )

--- drf-server/apps/facilities/models/__init__.py
+++ drf-server/apps/facilities/models/__init__.py
@@ -1,11 +1,11 @@
 from .facility import Facility
 from .devices import GasSensor, PowerDevice
-from .thresholds import LegalThreshold, FacilityThreshold
+# from .thresholds import LegalThreshold, FacilityThreshold - 4차 고도화
 
 __all__ = [
     "Facility",
     "GasSensor",
     "PowerDevice",
-    "LegalThreshold",
-    "FacilityThreshold",
+    # "LegalThreshold",
+    # "FacilityThreshold", 4차
 ]
